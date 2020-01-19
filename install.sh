67610d14735845a56b897fa5ee066f750d446f61
Nathan DECHER <nathan.decher@gmail.com>
2020-01-20 00:51:24 +0100

    added service and cronjob

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ./make.sh
-sudo cp container /usr/local/sbin/container
-sudo chown root:root /usr/local/sbin/container
+sudo install -o root -g root -m 755 container /usr/local/sbin/container
+sudo install -o root -g root -m 644 container.cron /etc/cron.d/container
+sudo install -o root -g root -m 644 container.service /etc/systemd/system/container.service
+sudo systemctl enable container.service
